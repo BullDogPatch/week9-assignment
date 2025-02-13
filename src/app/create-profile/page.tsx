10cb92c10080a8c851ba43e194f68301c16eaa05
Craig Clayton <craig__clayton@outlook.com>
2025-02-13 16:34:37 +0000

    got sign-in and sign-out redirecting

--- src/app/create-profile/page.tsx
+++ src/app/create-profile/page.tsx
@@ -1,4 +1,8 @@
-const CreateProfilePage = () => {
+import { auth, currentUser } from '@clerk/nextjs/server';
+
+const CreateProfilePage = async () => {
+  const { userId } = await auth();
+  console.log(userId);
   return <div>CreateProfilePage</div>;
 };
 
