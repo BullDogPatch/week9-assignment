a1257f175bf775138d60b143bb37bdfad897d83a
Craig Clayton <craig__clayton@outlook.com>
2025-02-13 15:53:44 +0000

    add Header into layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,19 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className='flex justify-between p-6'>
+      <h1>Social</h1>
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
