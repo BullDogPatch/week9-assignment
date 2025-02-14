969d421c91d2778507eb6004aa413f1aba1b4ae1
Craig Clayton <craig__clayton@outlook.com>
2025-02-14 14:02:08 +0000

    user not found page working, style later

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -1,4 +1,6 @@
+import { notFound } from 'next/navigation';
 import { fetchUser } from '@/utils/api';
+import { User } from '@/utils/types';
 
 type Params = {
   params: Promise<{ username: string }>;
@@ -6,11 +8,17 @@ type Params = {
 
 const UserPage = async ({ params }: Params) => {
   const { username } = await params;
-  const user = await fetchUser(username);
+  const user: User = await fetchUser(username);
+  if (!user) notFound();
   console.log(user);
   return (
     <div>
       <p>Username: {user?.username}</p>
+      <div className='avatar'>
+        <div className='w-24 rounded-full'>
+          <img src={user?.profile_url} />
+        </div>
+      </div>
     </div>
   );
 };
