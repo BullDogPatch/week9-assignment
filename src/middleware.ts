6edf132370fe1175acde89ae700e6b7324673e21
Craig Clayton <craig__clayton@outlook.com>
2025-02-14 15:10:20 +0000

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 //we will use this to determine which paths are public and which paths are protected
 //find a match for the route user-profile and any nested route inside too
 //we are focusing on finding matches for protected routes
-const isProtectedRoute = createRouteMatcher(['/user(.*)']);
+const isProtectedRoute = createRouteMatcher(['/user-profile(.*)']);
 
 export default clerkMiddleware(async (auth, req) => {
   //we will specific that we want ot protect certain paths
