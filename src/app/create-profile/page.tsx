229dcb716ee97d71abd3764bf60fe04916bc8bf0
Craig Clayton <craig__clayton@outlook.com>
2025-02-14 10:49:08 +0000

    basic create user form seems to do something in supabase

--- src/app/create-profile/page.tsx
+++ src/app/create-profile/page.tsx
@@ -1,9 +1,50 @@
-import { auth, currentUser } from '@clerk/nextjs/server';
+import { createuser } from '@/utils/actions';
+import { auth } from '@clerk/nextjs/server';
 
 const CreateProfilePage = async () => {
   const { userId } = await auth();
-  console.log(userId);
-  return <div>CreateProfilePage</div>;
+  console.log(typeof userId);
+  return (
+    <form action={createuser} className='flex flex-col'>
+      <input type='hidden' name='clerk_id' value={userId ?? ''} />
+      <input
+        type='text'
+        placeholder='first name'
+        name='first_name'
+        className='input input-bordered w-full max-w-xs'
+      />
+      <input
+        type='text'
+        placeholder='last name'
+        name='last_name'
+        className='input input-bordered w-full max-w-xs'
+      />
+      <input
+        type='text'
+        placeholder='username'
+        name='username'
+        className='input input-bordered w-full max-w-xs'
+      />
+      <input
+        type='text'
+        placeholder='profile pic'
+        name='src'
+        className='input input-bordered w-full max-w-xs'
+      />
+      <input
+        type='text'
+        placeholder='location'
+        name='location'
+        className='input input-bordered w-full max-w-xs'
+      />
+      <textarea
+        className='textarea textarea-bordered resize-none'
+        placeholder='Bio'
+        name='bio'
+      ></textarea>
+      <button type='submit'>Submit</button>
+    </form>
+  );
 };
 
 export default CreateProfilePage;
---
+++ src/utils/actions.ts
@@ -0,0 +1,18 @@
+'use server';
+
+import { db } from './dbConnection';
+
+export const createuser = async (formData: FormData) => {
+  const id = formData.get('clerk_id');
+  const firstName = formData.get('first_name');
+  const lastName = formData.get('last_name');
+  const username = formData.get('username');
+  const src = formData.get('src');
+  const location = formData.get('location');
+  const bio = formData.get('bio');
+
+  await db.query(
+    `INSERT INTO users (first_name, last_name, username, profile_url, bio, location, clerk_id) VALUES ($1, $2, $3, $4, $5, $6, $7)`,
+    [firstName, lastName, username, src, bio, location, id]
+  );
+};
