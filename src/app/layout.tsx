248f60fa212edfcd3fcb8cc5e6e205065be12a8c
Craig Clayton <craig__clayton@outlook.com>
2025-02-15 14:02:29 +0000

    add considerations readme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
-import { ClerkProvider } from '@clerk/nextjs';
-import './globals.css';
 import type { Metadata } from 'next';
+import { ClerkProvider } from '@clerk/nextjs';
 import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
 import Header from '@/components/Header';
+import './globals.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
